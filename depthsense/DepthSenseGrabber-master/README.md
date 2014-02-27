ff69e85b1ed987f0be10a2abc1046fcbcf794006
Abdi Dahir <abdi.dahir@outlook.com>
2014-02-27 13:37:49 -0500

    Depthsense camera c++ code. Requires opencv and depthsense 1.4 installed

---
+++ depthsense/DepthSenseGrabber-master/README.md
@@ -0,0 +1,14 @@
+DepthSenseGrabber
+===============
+
+Various tools for SoftKinetic DepthSense 325 camera using Linux SDK
+
+So far, DepthSenseGrabber allows you to:
+ - visualize camera streams (both color and depth)
+ - perform spatial synchronization using UVmap
+ - export frames in JPG, PNM (raw), or PCD (point clouds)
+
+Three versions available:
+ - DepthSenseGrabberCV (requires OpenCV): allows visualization, JPG/PNM export
+ - DepthSenseGrabberPCL (requires the Point Cloud Library): no online visualization, PCD export
+ - DepthSenseGrabberSO (stand alone): no online visualization, PNM export
---
+++ depthsense/DepthSenseGrabber-master/shared/ConversionTools.hxx
@@ -0,0 +1,34 @@
+#ifdef _MSC_VER
+#include <windows.h>
+#endif
+
+#include <stdio.h>
+#include <time.h>
+
+#include <vector>
+#include <exception>
+
+#include <DepthSense.hxx>
+
+using namespace DepthSense;
+using namespace std;
+
+void calcDepthToPosMat(float* depthToPosMatX, float* depthToPosMatY, int fovHorizontalDeg, int fovVerticalDeg, int width, int height);
+float packRGB(uint8_t* rgb);
+int packRGBA(uint8_t* rgb);
+
+void uvToColorPixelInd(UV uv, int widthColor, int heightColor, int* colorPixelInd, int* colorPixelRow, int* colorPixelCol);
+FrameFormat formatName(int resType);
+int formatResX(int resType);
+int formatResY(int resType);
+
+void saveRawColorFrame(char* fileName, uint8_t* pixels, int width, int height, int timeStamp);
+void saveRawDepthFrame(char* fileName, uint16_t* pixels, int width, int height, int timeStamp);
+
+void doubleSizeDepth(uint16_t* src, uint16_t* dst, int srcWidth, int srcHeight);
+void doubleSizeUV(UV* src, UV* dst, int srcWidth, int srcHeight);
+
+void rescaleMap(uint16_t* src, uint16_t* dst, int srcWidth, int srcHeight, int dstWidth, int dstHeight);
+void rescaleMap(float* src, float* dst, int srcWidth, int srcHeight, int dstWidth, int dstHeight);
+void rescaleMap(UV* src, UV* dst, int srcWidth, int srcHeight, int dstWidth, int dstHeight);
+void rescaleMap(FPVertex* src, FPVertex* dst, int srcWidth, int srcHeight, int dstWidth, int dstHeight);
