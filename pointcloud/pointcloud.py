c434ed6760dc1a442dab934564a2fe29ffb87303
Abdi Dahir <abdi.dahir@outlook.com>
2014-04-06 14:16:17 -0400

    updated point cloud with spherical movements from mouse

--- pointcloud/pointcloud.py
+++ pointcloud/pointcloud.py
@@ -2,9 +2,17 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
-import DepthSense as ds
+from head import Head
 import numpy as np
-ds.initDepthSense()
+from math import sin,cos,tan,radians,pi
+import DepthSense as ds
+
+zmov = 0
+xmov = 0
+prevz = 0
+prevx = 0 
+theta = pi/2
+phi = 0
 
 def on_timer(value=0):
         glutPostRedisplay();
@@ -12,27 +20,67 @@ def on_timer(value=0):
 
 
 def initFun():
-    glMatrixMode(GL_PROJECTION)
-    glClearColor(1.0,1.0,1.0,0.0)
-    glColor3f(0.0,0.0, 0.0)
-    glPointSize(4.0)
+
+    # Create window
+    glutInit()    
+    glutInitWindowSize(640,480)
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH)
+    glEnable(GL_DEPTH_TEST)
+    glutCreateWindow("Point Cloud")
+
+def cameraFun():
+
+    # Create Camera
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
+    glViewport(0, 0, 640, 480)
+    #glClearDepth(1) 
+    #glClear(GL_COLOR_BUFFER_BIT)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
+    gluPerspective(45, 640.0/480.0, 0.1, 2000) 
+    gluLookAt(0,0,10, 
+            sin(theta)*cos(phi),
+            sin(theta)*sin(phi),
+            cos(theta) - 1, 
+            0,1,0) 
+
     #gluOrtho2D(0.0,400.0,0.0,400.0)
     #glOrtho(-320,320,-240,240,0.1,1000)
-    gluPerspective(45, 640.0/480.0, 0.1, 1000) 
-    #gluLookAt(0,0,1, 0,0,0, 0,1,0) 
-    
 
-    
-def displayFun():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glClearColor(1.0,1.0,1.0,0.0)
+    glColor3f(0.0,0.0, 0.0)
+    glPointSize(3.0)
+
+def motionFun(x, y):
+    #print "moving x: ", x, " moving y: ", y
+    global zmov, prevz, theta
+    global xmov, prevx, phi 
+    print "moving x: ", xmov, " moving y: ", zmov
+
+    xmov = x/640.0 * pi
+    zmov = y/480.0 * pi
+
+    theta = zmov
+    phi = xmov
+
+
+
+def keyFun(key, x, y):
+    print "key x: ", x, " key y: ", y, " w/e: ", key
+         
+def displayFun():
+    
+    cameraFun()
+
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
     
     vertex = ds.getVertices()
     sync = ds.getSyncMap()
 
+    glPushMatrix()
     glBegin(GL_POINTS)
     for i in range(0,240):
         for j in range(0, 320):
@@ -45,23 +93,25 @@ def displayFun():
                 #glVertex3s(v[0], v[1], v[2])
                 glVertex3s(v[0], v[1], v[2]*-1)
                 #glVertex2s(v[0] + 320, v[1] + 240)
-
-      
-    
     glEnd()
+    glPopMatrix()
     glutSwapBuffers()
+
 if __name__ == '__main__':
-    glutInit()    
-    glutInitWindowSize(640,480)
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH)
-    glEnable(GL_DEPTH_TEST)
-    glutCreateWindow("Point Cloud")
 
-    glutDisplayFunc(displayFun)
+    try:
+        ds.initDepthSense()
+        head = Head()
 
-    initFun()
-    on_timer()
-    glutMainLoop()
+        initFun()
+        glutPassiveMotionFunc(motionFun)
+        glutKeyboardFunc(keyFun)
+        glutDisplayFunc(displayFun)
+        on_timer()
+        glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS)
+        glutMainLoop()
+    finally:
+        ds.killDepthSense()
 
 
 
